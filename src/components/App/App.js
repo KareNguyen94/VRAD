0687e07a3604279815f796d9a512176e9bbc67fe
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-03-24 13:46:23 -0600

    Add fetch for areas data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,30 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      areas: []
     }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/areas')
+      .then(response => response.json())
+      .then(areaData => {
+        const areaPromises = areaData.areas.map(area => {
+          return fetch('http://localhost:3001' + area.details)
+            .then(response => response.json())
+            .then(detail => {
+              return {
+                shortname: area.area,
+                name: detail.name,
+                description: detail.about
+              }
+            })
+            .catch(error => console.log(error))
+        })
+        return Promise.all(areaPromises);
+      })
+      .then(areas => this.setState({ areas }))
+      .catch(error => console.log(error))
   }
 
   render () {
