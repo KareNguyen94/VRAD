b2ae919d3d49279f95db98966ceb5ed529d68e43
Karen Nguyen <knguyen4184@yahoo.com>
2020-03-30 16:02:43 -0600

    Test favorite button

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Login from './Login';
 
 describe('Login', () => {
@@ -16,4 +16,34 @@ describe('Login', () => {
       expect(getByLabelText('Other')).toBeInTheDocument();
       expect(getByText('Login')).toBeInTheDocument();
     });
-})
+
+  it('should not run login function when there are no inputs', () => {
+    const mockLogin = jest.fn()
+    const { getByText } = 
+    render(
+      <Router>
+        <Login loginUser={mockLogin}/>
+      </Router>
+    );
+
+    fireEvent.click(getByText('Login'))
+    expect(mockLogin).not.toHaveBeenCalled();
+  })
+
+  it('should run login function when inputs are filled in', () => {
+    const mockLogin = jest.fn();
+    const mockUser = {name: 'mockName', email: 'mockEmail@yahoo.com', purpose: 'vacation', favorites: []}
+    const { getByText, getByPlaceholderText, getByLabelText } = 
+    render(
+      <Router>
+        <Login loginUser={mockLogin}/>
+      </Router>
+    );
+
+    fireEvent.change(getByPlaceholderText('Your name'), {target: {value: 'mockName'}});
+    fireEvent.change(getByPlaceholderText('Your email address'), {target: {value: 'mockEmail@yahoo.com'}});
+    fireEvent.click(getByLabelText('Vacation'));
+    fireEvent.click(getByText('Login'));
+    expect(mockLogin).toHaveBeenCalledWith(mockUser);
+  });
+});
